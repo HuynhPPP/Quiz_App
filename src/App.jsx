57a69cf6a5b8fade4631b764723e259172a14c9b
HuynhPPP <tankhuong02@email.com>
2025-09-26 15:52:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from "./components/Quiz"
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Quiz Vui</h1>
+      <Quiz />
+      
+    </div>
+  )
+}
+
+export default App
